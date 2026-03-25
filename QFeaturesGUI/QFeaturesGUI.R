2318de0fd96301e6bb94cfa2a01c5c4d57daafe9
leopoldguyot <leopold.guyot13@gmail.com>
2026-03-25 16:52:17 +0100

    add QFeaturesGUI

---
+++ QFeaturesGUI/QFeaturesGUI.R
@@ -0,0 +1,85 @@
+library(ggplot2)
+library(png)
+library(grid)
+library(hexSticker)
+
+#' @param x x offset of the hexagon's center
+#'
+#' @param y y offset of the hexagon's center
+#'
+#' @param radius the radius (side length) of the hexagon.
+#'
+#' @param from_radius from where should the segment be drawn? defaults to the center
+#'
+#' @param to_radius to where should the segment be drawn? defaults to the radius
+#'
+#' @param from_angle from which angle should we draw?
+#'
+#' @param to_angle to which angle should we draw?
+#'
+#' @param fill fill color
+#'
+#' @param color line color
+#'
+#' @param size size of the line?
+hex_segment2 <- function(x = 1, y = 1, radius = 1, from_radius = 0,
+                         to_radius = radius, from_angle = 30, to_angle = 90,
+                         fill = NA, color = NA, size = 1.2) {
+    from_angle <- from_angle * pi / 180
+    to_angle <- to_angle * pi / 180
+    coords <- data.frame(x = x + c(from_radius * cos(from_angle),
+                                   to_radius * cos(from_angle),
+                                   to_radius * cos(to_angle),
+                                   from_radius * cos(to_angle)),
+                         y = y + c(from_radius * sin(from_angle),
+                                   to_radius * sin(from_angle),
+                                   to_radius * sin(to_angle),
+                                   from_radius * sin(to_angle))
+                         )
+    geom_polygon(aes_(x = ~x, y = ~y), data = coords,
+                 fill = fill, color = color, size = size)
+}
+
+img <- readPNG("./images/QFeaturessanstext.png")
+img <- rasterGrob(img, width = 2.4, x = 0.45, y = 2.2,
+                       interpolate = TRUE)
+
+## Manually define...
+col_blue = "#246abe"
+col_grey = "#95959c"
+col_grey2 = "#838289" # The color after Gimp converting the color scheme
+col_purple = "#9200fc"
+col_orange = "#f4810b"
+col_yellow = "#fef14e"
+col_white = "#ffffff"
+col_off_black = "#1a1a1a"
+
+## Bioconductor version with larger font and Bioconductor URL
+hex2 <- ggplot() +
+    geom_hexagon(size = 1.2, fill = col_white, color = NA) +
+    hex_segment2(size = 0, fill = paste0(col_blue, 60),
+         from_radius = 0, to_radius = 1,
+         from_angle = 150, to_angle = 210) +
+    geom_polygon(data = data.frame(x = c(1, 1 + sqrt(3)/2, 1 + sqrt(3)/3),
+                   y = c(1, 1.5, 1.5 + 1/6)),
+         aes(x = x, y = y),
+         fill = paste0(col_yellow, 40)) +
+    geom_polygon(data = data.frame(x = c(1, 1 + sqrt(3)/3, 1 + sqrt(3)/6),
+                   y = c(1, 1.5 + 1/6, 1.5 + 1/3)),
+         aes(x = x, y = y),
+         fill = paste0(col_orange, 40)) +
+    geom_polygon(data = data.frame(x = c(1, 1 + sqrt(3)/6, 1),
+                   y = c(1, 1.5 + 1/3, 2)),
+         aes(x = x, y = y),
+         fill = paste0(col_purple, 40)) +
+    geom_hexagon(size = 1.2, fill = NA, color = col_grey) +
+    geom_subview(subview = img, x = 1, y = 0.88,
+         width = 1) +
+    geom_url("www.bioconductor.org",
+         size = 5.5,
+         color = col_grey2) +
+    geom_pkgname("QFeaturesGUI", y = 1.25, size = 14,
+         color = col_off_black, family = "serif") +
+    theme_sticker()
+save_sticker(filename = "baseSticker.png", hex2)
+ggsave(filename = "baseSticker.png", plot = hex2, width = 43.9, height = 50.8, units = "mm", bg = "transparent")
